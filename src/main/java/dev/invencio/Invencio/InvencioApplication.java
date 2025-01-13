df2eafc01cc1bb9692c3ec27664d767e6af0849a
Biswajeet Mishra <cdt.king75@gmail.com>
2025-01-13 12:06:53 +0530

    Initial commit

---
+++ src/main/java/dev/invencio/Invencio/InvencioApplication.java
@@ -0,0 +1,13 @@
+package dev.invencio.Invencio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvencioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvencioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/invencio/Invencio/InvencioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.invencio.Invencio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvencioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
